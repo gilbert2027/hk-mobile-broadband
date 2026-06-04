7c475dac7a518075b40c70248c1dc2286c24d5c9
gilbert2027 <lauchunlok2006@yahoo.com.hk>
2026-06-04 20:15:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "網站成功運行"
+    return """
+    <h1>香港手機及寬頻比較</h1>
+
+    <a href="/mobile">
+    手機月費比較
+    </a>
+
+    <br><br>
+
+    <a href="/broadband">
+    家居寬頻比較
+    </a>
+    """
 
 if __name__ == "__main__":
     app.run()
