2056039f2cd2d373e2ff61e05ad6c9fabc061a54
gilbert2027 <lauchunlok2006@yahoo.com.hk>
2026-06-06 14:13:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1788,29 +1788,6 @@ def test():
 
 
 
-
-@app.route("/sitemap.xml")
-def sitemap_xml():
-    xml = """<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url>
-    <loc>https://hk-mobile-broadband.vercel.app/</loc>
-    <lastmod>2026-06-06</lastmod>
-    <priority>1.00</priority>
-  </url>
-</urlset>"""
-
-    return Response(xml, mimetype="application/xml")
-
-@app.route("/robots.txt")
-def robots_txt():
-    text = """User-agent: *
-Allow: /
-
-Sitemap: https://hk-mobile-broadband.vercel.app/sitemap.xml
-"""
-    return Response(text, mimetype="text/plain")
-
     
 # =========================
 # Run
